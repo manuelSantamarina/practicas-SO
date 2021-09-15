9e0449fcd07cbcecca39046f848f7772ff9f5c05
manuelSantamarina <manuel.santamarina@udc.es>
2021-09-15 16:17:26 +0200

    leerEntrada() done

--- practica0-shell.c
+++ practica0-shell.c
@@ -8,14 +8,18 @@ void imprimirPrompt(){
     printf("%s@%s>",getlogin(),name);
 }
 void leerEntrada(){
+    char inputBuff[256];
+    printf("%s",fgets(inputBuff, 256, stdin));
     
 }
 void procesarEntrada();
+
 int main(){
     bool terminado = false;
     
     while(!terminado){
         imprimirPrompt();
+        leerEntrada();
     }
     return 0;  
 }
